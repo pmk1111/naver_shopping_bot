97b8597c079e066598916afd6e3426ccf6a97d9b
pmk1111 <kingpmk104@naver.com>
2024-01-17 15:16:14 +0900

    스크롤 & 페이지 이동 & 상품 정보 추출 구현

--- naver/naver_shopping_bot.py
+++ naver/naver_shopping_bot.py
@@ -3,6 +3,9 @@ from selenium.webdriver import ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+# find_element(s)로 요소를 찾지 못했을 때 발생하는 예외
+from selenium.common.exceptions import NoSuchElementException
+
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 # 시간과 관련된 다양한 기능을 제공하는 모듈. 시간을 측정하고 제어하는 데 사용
@@ -11,12 +14,16 @@ import time
 import random
 
 options = ChromeOptions()
+# 작업이 완료되어도 브라우저 유지
 options.add_experimental_option("detach", True)
+# 백그라운드에서도 작업 실행
+options.add_argument("headless")
+options.add_argument("--window-size=1920x1080")
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
-# 브라우저 창 최대화
-driver.maximize_window()
+# 브라우저를 Full Screen으로 연다.
+# driver.maximize_window()
 
 # 상품명을 입력받으면 네이버 쇼핑 이동. 공백 또는 아무 값도 입력받지 않았느면 올바른 값을 입력받을 때 까지 반복
 while True:
@@ -39,7 +46,7 @@ while True:
         print("숫자를 입력하세요.")
 
 # 3~5 사이의 난수 생성
-random_sec = random.uniform(3, 5)
+random_sec = random.uniform(2, 4)
 driver.implicitly_wait(10)
 
 # 네이버 쇼핑은 브라우저 크기 별로 검색 영역을 나타내는 태그가 다른 방식으로 표시된다.
@@ -77,7 +84,7 @@ try:
     limit = driver.find_elements(By.CSS_SELECTOR, 'div.subFilter_select_box__dX_vV')[1]
     if limit:
         limit.click()
-        driver.implicitly_wait(2)
+        driver.implicitly_wait(10)
         try:
             limit_cnt = driver.find_elements(By.CSS_SELECTOR, "div.subFilter_select_box__dX_vV.open ul li")
             time.sleep(random_sec)
@@ -94,3 +101,65 @@ try:
             print("페이지 당 상품 갯수 리스트를 찾지 못했습니다.", e)
 except Exception as e:
     print("페이지 당 상품 갯수 div를 찾지 못했습니다.")
+
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+time.sleep(2)
+
+# 스크롤 완료 후 => 현재 페이지의 상품 정보를 가져온 후 => 다음 페이지로 이동
+# 마지막 페이지인 경우 루프 중지
+while True:
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            print("스크롤 완료")
+            break
+        last_height = new_height
+
+    # 현재 페이지 상품 목록들
+    items = driver.find_elements(By.CSS_SELECTOR, ".basicList_list_basis__uNBZx > div > div")
+    print("가져온 상품 수: ", len(items))
+    time.sleep(2)
+
+    # 상품 정보 항목 추출
+    for item in items:
+        try:
+            product_info_grp = item.find_element(By.CSS_SELECTOR, ".product_info_area__xxCTi")
+            product_title = product_info_grp.find_element(By.CSS_SELECTOR, "a").text
+            product_url = product_info_grp.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+            product_price = product_info_grp.find_element(By.CSS_SELECTOR, ".price_num__S2p_v em").text
+            product_category_grp = product_info_grp.find_elements(By.CSS_SELECTOR, ".product_depth__I4SqY span")
+            # print("상품명: " + product_title)
+            # print("상품 판매 주소: ", product_url)
+            # print("상품 가격: ", product_price)
+
+            product_category = ""
+            for c in product_category_grp:
+                product_category += c.text
+            print("상품 카테고리: ", product_category)
+        except NoSuchElementException as e:
+            try:
+                product_info_grp = item.find_element(By.CSS_SELECTOR, ".adProduct_info_area__dTSZf")
+                product_title = product_info_grp.find_element(By.CSS_SELECTOR, "a").text
+                product_url = product_info_grp.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+                product_price = product_info_grp.find_element(By.CSS_SELECTOR, ".price_num__S2p_v em").text
+                product_category_grp = product_info_grp.find_elements(By.CSS_SELECTOR, ".adProduct_depth__s_IUT span")
+                # print("상품명: " + product_title)
+                # print("상품 판매 주소: ", product_url)
+                # print("상품 가격: ", product_price)
+
+                product_category = ""
+                for c in product_category_grp:
+                    product_category += c.text
+                print("상품 카테고리: ", product_category)
+            except NoSuchElementException as e:
+                print("상품 정보 영역을 찾는 데 문제가 발생했습니다.")
+    try:
+        next_page = driver.find_element(By.CSS_SELECTOR, ".pagination_next__pZuC6")
+        next_page.click()
+    except NoSuchElementException as e:
+        print("마지막 페이지 입니다.")
+        break  
